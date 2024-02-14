2f53d7da6b16ef6407b0fbb26b469806e574210d
IvanYuryk <143449289+IvanYuryk@users.noreply.github.com>
2024-02-14 21:11:58 +0100

    'update'

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ async function onLoadMore() {
 function smoothScroll() {
   const { height } = imageList.firstElementChild.getBoundingClientRect();
   window.scrollBy({
-    top: height * 2.5,
+    top: height * 2,
     behavior: "smooth",
   });
 }
